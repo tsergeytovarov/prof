3065b8d147b00bbc6babbe2ef0ffc1931a9cee38
tsergeytovarov <tovarov.piter@gmail.com>
2015-06-13 12:33:53 +0400

    Главная страница готова

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,17 @@
-$(function() {
+function initialize() {
+  var mapOptions = {
+    zoom: 11,
+    center: new google.maps.LatLng(55.856152, 49.073131)
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+  var image = 'img/map-point.png';
+  var myLatLng = new google.maps.LatLng(55.817786, 49.259556);
+  var mapMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image
+  });
+}
 
-});
+google.maps.event.addDomListener(window, 'load', initialize);
