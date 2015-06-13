fc635b058aaf3db32f125f74dd94b78b5b5ee867
tsergeytovarov <tovarov.piter@gmail.com>
2015-06-13 14:32:35 +0400

    Оптимизировал графику и поставил больше кроссбразуерности

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,7 +32,7 @@ module.exports = function(grunt) {
     // автопрефиксер
     autoprefixer: {
       options: {
-        browsers: ['last 2 versions', 'ie 9'],
+        browsers: ['last 15 versions', 'ie 9'],
         map: true,
       },
       style: {
